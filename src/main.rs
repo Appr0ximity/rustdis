dd1c6760ceee476e457e13f2e0aba8d820ccf646
Appr0ximity <hvshekharmurthy@gmail.com>
2026-01-09 22:26:01 +0530

    feat: Added concurrency using threads for now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::{io::{Read, Write}, net::TcpListener};
+use std::{io::{Read, Write}, net::TcpListener, thread, time::Duration};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,17 +7,32 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buffer = [0; 1024];
-                if let Ok(_read) = stream.read(&mut buffer){
-                    stream.write_all(b"+PONG\r\n").unwrap();
+        thread::spawn(||{
+            match stream {
+                Ok(mut stream) => {
+                    loop{
+                        let mut buffer = [0; 1024];
+                        if let Ok(read) = stream.read(&mut buffer){
+                            let input = String::from_utf8_lossy(&buffer[..read]); 
+                            if let Some(index) = input.find("ECHO"){
+                                if let Some(output) = input.get(index+6..){
+                                    if let Err(_e) = stream.write_all(output.as_bytes()){
+                                        break ;
+                                    }
+                                }
+                            }else if let Some(_index) = input.find("PING"){
+                                let output = b"+PONG\r\n";
+                                if let Err(_e) = stream.write_all(output){
+                                    break ;
+                                }
+                            }
+                        }
+                    }
                 }
-
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
