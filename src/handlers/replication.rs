ef644c5268bf0e90c6dab7f3ab6e70983ef68f29
Appr0ximity <hvshekharmurthy@gmail.com>
2026-01-20 19:17:46 +0530

    feat: Added sending file after FULLSYNC

--- src/handlers/replication.rs
+++ src/handlers/replication.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, fs::read, sync::Arc};
 
 use tokio::sync::Mutex;
 
@@ -10,8 +10,14 @@ pub async fn handle_replconf()-> String{
 
 pub async fn handle_psync(server_info_clone: &Arc<Mutex<HashMap<String, String>>>)->String{
     let server_info_map = server_info_clone.lock().await;
-    let output;
+    let fullresync;
     let repl_id = server_info_map.get("master_replid").unwrap();
-    output = "FULLRESYNC ".to_string() + repl_id + " 0";
-    simple_string(&output)
+    fullresync = "FULLRESYNC ".to_string() + repl_id + " 0";
+    let rdb_bytes = read("empty.rdb").expect("Failed to read empty.rdb");
+    let mut output = simple_string(&fullresync);
+    output.push_str(&format!("${}\r\n", rdb_bytes.len()));
+    unsafe {
+        output.push_str(&String::from_utf8_unchecked(rdb_bytes));
+    }
+    output
 }
